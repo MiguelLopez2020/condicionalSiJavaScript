7bce970ab0e1a5b3b4e50202f76b49b43fd99a5e
MiguelLopez2020 <miguel_angel_lopez_s@hotmail.com>
2021-04-19 13:15:38 -0500

    Cambios de diseño al ejercicio

--- ejercicio1/ejercicio.js
+++ ejercicio1/ejercicio.js
@@ -1,15 +1,15 @@
 
-var edad = prompt("digite la edad")
+var edad = prompt("Digite la edad")
 
 if (edad > 70) {
-    document.write("Persona de la tercera edad")
+    document.write("Persona de la tercera edad, ")
 } else {
-    document.write("aun eres joven@")
+    document.write("Aun eres un jovencit@, ")
 }
-var nombre = prompt("digite su nombre")
-var telefono = prompt("digite su telefono")
+var nombre = prompt("Digite su nombre")
+var telefono = prompt("Digite su telefono")
 if (edad = ' ') {
-    document.write("por favor introduce tu nombre y telefono")
+    document.write("recuerda por favor introduce tu nombre y telefono")
 }else{
     document.write('')
 }
